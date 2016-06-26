d24831f8f78bd0b0559c704c9f8cf5276b8ab5fe
董玉毛 <153100950@qq.com>
2016-06-26 21:30:44 +0800

    GCD 主要分 同步函数、异步函数。同步不可以创建线程，异步可以。队列分    并发队列--同步队列

--- 多线程使用方法/GCDViewController.m
+++ 多线程使用方法/GCDViewController.m
@@ -19,19 +19,134 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self asynvMainQueue];
 }
+
+//异步 -- 主队列中执行（主队列是串行）
+- (void)asynvMainQueue{
+    //获取主队列 （添加到主队列的任务，都会放在主线程中执行）
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    //将任务添加到队列中,异步 执行
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片1------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片2------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片3------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片4------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片5------%@", [NSThread currentThread]);
+    });
+
+}
+
+//同步 -- 并发队列
+- (void)syncGlobalQueue{
+    //获取全局并发队列
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //将任务添加到队列中,同步执行
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片1------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片2------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片3------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片4------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片5------%@", [NSThread currentThread]);
+    });
+}
+
+//同步 -- 串行队列
+- (void)syncSerialQueue{
+    //创建一个串行队列
+    dispatch_queue_t queue = dispatch_queue_create("队列名称", NULL);
+    //将任务添加到队列中,同步执行
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片1------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片2------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片3------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片4------%@", [NSThread currentThread]);
+    });
+    dispatch_sync(queue, ^{
+        NSLog(@"------下载图片5------%@", [NSThread currentThread]);
+    });
+}
+
+// 异步-并发队列
+- (void)asyncGlobalQueue{
+    //获取全局并发队列
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //将任务添加到队列中,异步执行
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片1------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片2------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片3------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片4------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片5------%@", [NSThread currentThread]);
+    });
+}
+
+//异步 -- 串行队列
+- (void)asyncSerialQueue{
+    //创建一个串行队列
+    dispatch_queue_t queue = dispatch_queue_create("队列名称", NULL);
+    //将任务添加到队列中,异步执行
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片1------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片2------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片3------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片4------%@", [NSThread currentThread]);
+    });
+    dispatch_async(queue, ^{
+        NSLog(@"------下载图片5------%@", [NSThread currentThread]);
+    });
+}
+
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
+dispatch_async :异步，具有开辟线程的能力
+dispatch_sync  :同步，不具有开辟线程的能力
+ 
+并发队列：多个任务同时执行
+串行队列：一个任务执行完之后，再执行下一个任务
+ 
+是否开辟新的线程只取决于是 同步函数 还是 异步函数
+ 
+//获取全局并发队列
+   dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+ 
 */
 
 @end
